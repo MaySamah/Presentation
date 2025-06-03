14922e89283ba9c0cb9f0a411aadd75f90534e59
MaySamah <SamahAMMustafa@gmail.com>
2025-06-03 14:16:31 +0100

    push

---
+++ script2.js
@@ -0,0 +1,48 @@
+// Alert Hello World
+alert ("Hello World");
+
+// Using prompt to popup and take input and return data
+let name = prompt ("Hello", "");
+if (name != null)
+    {
+        document.getElementById("demo").innerHTML = "Hello " + name + ".";
+    }
+
+// Activity Variables Research
+let fname = prompt ("What is your first name?")
+let age = prompt ("How old are you?")
+alert ("Hello " + fname + " who is " + age + " years old.");
+
+let breakfast = "toast";
+let lunch = "sandwich";
+let dinner = "curry";
+console.log("Today i had " + breakfast + " for breakfast, a " + lunch + " for lunch, and " + dinner + " for dinner.");
+
+// Activity 2
+let fname1 = "May";
+let age1 = "28";
+let fshow = "Yellow Stone"
+console.log("I am " + age1 + " who loves " + fshow1);
+
+// Activity 3 Yesterday Meals
+let breakfast1 = "Toastie";
+let lunch1 = "Bortch";
+let dinner1 = "Chicken and Sweet Potato";
+console.log("Today i had " + breakfast1 + " for Breakfast, a " + lunch1 + " for Lunch, and " + dinner1 + " for Dinner.");
+breakfast = "Yogurt";
+console.log("Today i had " + breakfast1 + " for Breakfast, a " + lunch1 + " for Lunch, and " + dinner1 + " for Dinner.");
+
+
+let message = "Hello " + fname + " who is " + age + " years old.";
+console.log(message);
+
+// Activity Type Challenges
+let time = new Date("2025-04-22");
+console.log(time);
+
+// Activity Property & Method Challenge
+let myString = "Hello World"
+console.log(myString.length);
+console.log(myString.toUpperCase());
+console.log(myString.substring(5));
+
